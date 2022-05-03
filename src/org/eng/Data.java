fccabfcbd68a9290d7dc18927ba3d308f3bc63f9
PriyaMahi2 <priyamahi2010@gmail.com>
2022-05-03 14:54:57 +0530

    Few lines of code added

---
+++ src/org/eng/Data.java
@@ -0,0 +1,28 @@
+package org.eng;
+
+public class Data {
+	private int age;
+	private String name;
+	private long phno;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getPhno() {
+		return phno;
+	}
+	public void setPhno(long phno) {
+		this.phno = phno;
+	}
+	
+	
+
+}
---
+++ src/org/eng/Data2.java
@@ -0,0 +1,65 @@
+package org.eng;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class Data2 {
+	public static void main(String[] args) {
+		Data d = new Data();
+		d.setName("priya");
+		d.setAge(32);
+		d.setPhno(9840654846l);
+		Data d1=new Data();
+		d1.setName("mahi");
+		d1.setAge(40);
+		d1.setPhno(9094860486l);
+		Data d2 = new Data();
+		d2.setName("linu");
+		d2.setAge(11);
+		d2.setPhno(9840094846l);
+		List<Data> l = new LinkedList<Data>();
+		l.add(d);
+		l.add(d1);
+		l.add(d2);
+		Set<Data> s = new LinkedHashSet<Data>();
+		s.add(d);
+		s.add(d1);
+		s.add(d2);
+		Map<Integer,Data> m = new LinkedHashMap<>();
+		m.put(1,d);
+		m.put(2,d1);
+		m.put(3,d2);
+		Set<Entry<Integer, Data>> entrySet = m.entrySet();
+		for (Entry<Integer, Data> entry : entrySet) {
+			System.out.println(entry.getKey());
+			System.out.println(entry.getValue().getAge());
+			System.out.println(entry.getValue().getName());
+			System.out.println(entry.getValue().getPhno());
+			
+		}
+		
+		for (Data data : s) {
+			
+			System.out.println("Employee age: "+data.getAge());
+			System.out.println("Employee name: "+data.getName());
+			System.out.println("Employee phoneno: "+data.getPhno());
+		}
+		for (int i = 0; i < l.size(); i++) {
+			System.out.println("Employee no:"+(i+1));
+			System.out.println("name:"+l.get(i).getName());
+			System.out.println("Age:"+l.get(i).getAge());
+			System.out.println("Phno:"+l.get(i).getPhno());}}}
+				
+			
+			
+			
+		
+		
+		
+
